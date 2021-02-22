9d4661c9b09179c1763edc25630ed947115e773c
Psyche47 <16-32498-2@student.aiub.edu>
2021-02-22 13:46:41 +0600

    charCount solution added

---
+++ charCount.js
@@ -0,0 +1,25 @@
+function charCount(str) {
+  let obj = {};
+  for (let char of str) {
+    if (isAlphanumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+function isAlphanumeric(char) {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper Alpha (A-Z)
+    !(code > 96 && code < 123)
+  ) {
+    // lower Alpha (a-z)
+    return false;
+  }
+  return true;
+}
+
+console.log(charCount("Hello World!"));
