9f00a4264047f81df11e7559b52d05764c0dd52f
Psyche47 <16-32498-2@student.aiub.edu>
2021-06-09 01:16:48 +0600

    Problem description added

--- exesAndOhs.js
+++ exesAndOhs.js
@@ -1,3 +1,8 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. 
+The string can contain any char.
+*/
 function XO(str) {
   if (str == "" || str == null) return true;
   let count = 0;
